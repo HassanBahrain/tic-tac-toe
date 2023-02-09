c8c9531de0dd3f1a1edf1d4ebb8e1157f0e02f20
hassan khushbakhat~ <hassankhushbakhat@gmail.com>
2023-02-09 09:37:40 -0800

    final work

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,13 @@
 function init() {
 
-
     let cells = document.querySelectorAll('.box')
-    // const button = document.querySelector('.btn')
-    const player1 = "X"
+    const button = document.querySelector('.btn')
+    const player1 = "x"
     const player2 = "O"
-    let player1List = []
-    let player2List = []
-    let playTurn = []
     const restartbutton = document.querySelector('.btn')
     let EmptySpaces = Array(9).fill('')
+    let Winning = false
+    let winingmessage =document.querySelector('.score')
     ///always put coma's
     const WINCon = [
         [0, 1, 2],
@@ -29,12 +27,10 @@ var isWinningConditionMet = false
         // console.log(event.target.id)
     }
 
-
-
     function clicked(event) {
         const id = event.target.id
 
-        console.log("taco")
+        // console.log("taco")
         console.log(id)
         // event.target.textContent = 'x'
 
@@ -57,144 +53,148 @@ var isWinningConditionMet = false
 
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    function ResetGame(event) {
-        // console.log("why")
-        cells.forEach(box => {
-            // console.log("Green")
-            box.textContent = ""
-        })
-
-
-    }
-    restartbutton.addEventListener('click', ResetGame)
+   
     cells.forEach(cell => {
         cell.addEventListener('click', clicked)
+
     })
-
-
-    //win condtin working 
-    // if (currentPlayer === player1){
-    //     event.target.textContent = 'x'
-    //     currentPlayer = player2;
-
-
-    // WINCon.forEach(condition => {
-    //     let count = 0
-    //     condition.forEach(cell => {
-    //         if (player1List.includes(cell)) count++
-    //     })
-    //     if (count === 3) {
-    //         console.log('Player 1 wins!')
-    //         isWinningConditionMet = true
-    //     }
-    //     count = 0
-    //     condition.forEach(cell => {
-    //         if (player2List.includes(cell)) count++;
-    //     })
-    //     if (count === 3) {
-    //         console.log('Player 2 wins!')
-    //         isWinningConditionMet = true
-    //     }
-    //     if (isWinningConditionMet) {
-    //         console.log('The game has ended, there is a winner!')
-    //         cells.forEach(cell => {
-    //             cell.removeEventListener('click', playTurn)
-    //         })
-    //     }
-    // })
-    
-
-
 
 
      function checkWin(){
         console.log(cells[0])
         // console.log(cells[0].innerHTML)
+        //wining condition of "X player"
         if(cells[0].innerHTML === 'x' && cells[1].innerHTML === 'x' && cells[2].innerHTML === 'x') {
             console.log('x has won')
+            Winning = true
+            winingmessage.innerHTML ='x won'
         }
             
-        else if (game[3] === 'x' && game[4]==='x' && game[5] === 'x'){
+        else if (cells[3].innerHTML === 'x' && cells[4].innerHTML==='x' && cells[5].innerHTML === 'x'){
             console.log('x has won')
+            Winning = true
+            winingmessage.innerHTML ='x won'
+        }
+
+        else if (cells[6].innerHTML=== 'x' && cells[7].innerHTML==='x' && cells[8].innerHTML === 'x'){
+            console.log('x has won')
+            Winning = true
+            winingmessage.innerHTML ='x won'
+        }
+
+
+
+        else if (cells[0].innerHTML=== 'x' && cells[3].innerHTML==='x' && cells[6].innerHTML === 'x'){
+            console.log('x has won')
+            Winning = true
+            winingmessage.innerHTML ='x won'
+        }
+        else if (cells[1].innerHTML=== 'x' && cells[4].innerHTML==='x' && cells[7].innerHTML === 'x'){
+            console.log('x has won')
+            Winning = true
+            winingmessage.innerHTML ='x won'
+        }
+        else if (cells[2].innerHTML=== 'x' && cells[5].innerHTML==='x' && cells[8].innerHTML === 'x'){
+            console.log('x has won')
+            Winning = true
+            winingmessage.innerHTML ='x won'
+        }
+
+
+
+
+        else if (cells[0].innerHTML=== 'x' && cells[4].innerHTML==='x' && cells[8].innerHTML === 'x'){
+            console.log('x has won')
+            Winning = true
+            winingmessage.innerHTML ='x won'
+        }
+        else if (cells[0].innerHTML=== 'x' && cells[4].innerHTML==='x' && cells[6].innerHTML === 'x'){
+            console.log('x has won')
+            Winning = true
+            winingmessage.innerHTML ='x won'
+        }
+        // else {
+        //     winingmessage.innerHTML ='dr'
+        // }
+
+
+
+
+        /// O PLAYER CODE WINNING CONDTITION
+
+        if(cells[0].innerHTML === '0' && cells[1].innerHTML === '0' && cells[2].innerHTML === '0') {
+            console.log('O has won')
+            Winning = true
+            winingmessage.innerHTML ='O won'
+
+        }
+        else if (cells[3].innerHTML === '0' && cells[4].innerHTML==='0' && cells[5].innerHTML === '0'){
+            console.log('0 has won')
+            Winning = true
+            winingmessage.innerHTML ='O won'
+        }
+        else if (cells[6].innerHTML === '0' && cells[7].innerHTML==='0' && cells[8].innerHTML === 'O'){
+            console.log('0 has won')
+            Winning = true
+            winingmessage.innerHTML ='O won'
+        }
+        else if (cells[0].innerHTML === 'O' && cells[3].innerHTML==='O' && cells[6].innerHTML === 'O'){
+            console.log('0 has won')
+            Winning = true
+            winingmessage.innerHTML ='O won'
+        }
+
+        else if (cells[1].innerHTML === 'O' && cells[4].innerHTML==='O' && cells[7].innerHTML === 'O'){
+            console.log('0 has won')
+            Winning = true
+            winingmessage.innerHTML ='O won'
+        }
+
+        else if (cells[2].innerHTML === 'O' && cells[5].innerHTML==='O' && cells[8].innerHTML === 'O'){
+            console.log('0 has won')
+            Winning = true
+            winingmessage.innerHTML ='O won'
+        }
+        else if (cells[0].innerHTML === 'O' && cells[4].innerHTML==='O' && cells[8].innerHTML === 'O'){
+            console.log('0 has won')
+            Winning = true
+            winingmessage.innerHTML ='O won'
         }
         
+        else if (cells[2].innerHTML === 'O' && cells[4].innerHTML==='O' && cells[6].innerHTML === 'O'){
+            console.log('0 has won')
+            Winning = true
+            winingmessage.innerHTML ='O won'
+        }
 
-   
+        if (Winning === true){
+            cells.forEach(cell => {
+                cell.removeEventListener('click', clicked)
+            })
 
-        // if game[0] ===
-
-
-
-    //  }
-
-
-
-
-
-
-
+        }
+}
 
 
+function ResetGame() {
+    for( let i =0 ; i<cells.length ; i++){
+        cells[i].innerHTML = '' 
+      }
+      Winning=false
+      winingmessage.innerHTML=''
+        cells.forEach(cell => {
+            console.log("play agin")
+            cell.addEventListener('click', clicked)
+    
+        })
 
+        console.log("Plaayyy")
+     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    restartbutton.addEventListener('click', RESET)
-    function RESET(event) {
-
-    }
-
-
-
-
-
-
-
+    
 
 }
+button.addEventListener('click', ResetGame)
 }
 window.addEventListener('DOMContentLoaded', init)
 
